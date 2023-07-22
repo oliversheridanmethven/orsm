11a8a43820b9b1619c1fae82e1d98c422de6f45d
Dr Oliver Sheridan-Methven <oliver.sheridan-methven@hotmail.co.uk>
2023-07-22 21:51:31 +0200

    Better CLI testing.

--- src/cli/tests/cli_arguments_0.c
+++ src/cli/tests/cli_arguments_0.c
@@ -1,63 +1,27 @@
-#include <stdio.h>
-#include <argp.h>
-#include <wordexp.h>
-#include "testing/testing.h"
-#include "logging/logging.h"
+#include "cli_testing.h"
 
-wordexp_t words;
-
-struct params {
-    int argc;
-    char ** argv;
-} params;
-
-void initialise_params(char * args) {
-    LOG_INFO("Parsing the input string: %s", args);
-    char *command_prefix = "command ";
-    char *merged = malloc(strlen(command_prefix) + strlen(args) + 1);
-    if (!merged) {
-        LOG_ERROR("Couldn't allocate enough space for the command string.");
-    }
-    strcpy(merged, command_prefix);
-    strcat(merged, args);
-    args = merged;
-
-    switch (wordexp(args, &words, 0))
-    {
-        case 0:
-          LOG_INFO("Successfully parsed the input string: %s", args);
-          break;
-        default:
-          LOG_ERROR("Failed trying to parse the input string: %s", args);
-          break;
-    }
-    params.argc = words.we_wordc;
-    params.argv = words.we_wordv;
-}
-
-void finalise_params(void) {
-   wordfree(&words);
-}
-
-TestSuite(cli, .init=show_all_logging);
-
-Test(cli, nothing, .fini=finalise_params) {
+Test(cli, nothing) {
     initialise_params("");
     argp_parse(0, params.argc, params.argv, 0, 0, 0);
 }
 
-Test(cli, help, .fini=finalise_params) {
+Test(cli, help) {
     initialise_params("--help");
     argp_parse(0, params.argc, params.argv, 0, 0, 0);
 }
 
-Test(cli, usage, .fini=finalise_params) {
+Test(cli, usage) {
     initialise_params("--usage");
     argp_parse(0, params.argc, params.argv, 0, 0, 0);
 }
 
-Test(cli, extra_flag, .fini=finalise_params) {
+Test(cli, extra_flag) {
     initialise_params("--foo");
     cr_assert(ne(int, 0, argp_parse(0, params.argc, params.argv, ARGP_NO_EXIT, 0, 0)), "This shouldn't have parsed a flag which doesn't exist.");
+}
+
+Test(cli, case_sensitive) {
+    initialise_params("--Help");
+    cr_assert(ne(int, 0, argp_parse(0, params.argc, params.argv, ARGP_NO_EXIT, 0, &params.arguments)), "This should be case sensitive.");
 }
 
--- src/cli/tests/cli_arguments_version.c
+++ src/cli/tests/cli_arguments_version.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <argp.h>
+#include "cli_testing.h"
 #include "version/version.h"
+
+
+
+const char *argp_program_version = REPO_NAME " " REPO_VERSION;
+const char *argp_program_bug_address = REPO_AUTHOR " <" REPO_EMAIL ">";
+static char doc[] = "A small program to demonstrate using the command line interface.";
+
+
+Test(cli, test_version)
+{
+    initialise_params("--version");
+    struct argp argp = {.doc=doc};
+    argp_parse(&argp, params.argc, params.argv, 0, 0, &params.arguments);
+
+}
+
 
 static int parse_opt (int key, char *arg, struct argp_state *state)
 {
@@ -16,27 +30,14 @@ static int parse_opt (int key, char *arg, struct argp_state *state)
  return 0;
 }
 
-
-struct arguments {
-    enum { CHARACTER_MODE, WORD_MODE, LINE_MODE } mode;
-    bool isCaseInsensitive;
-};
-
-const char *argp_program_version = REPO_NAME " " REPO_VERSION;
-const char *argp_program_bug_address = REPO_AUTHOR " <" REPO_EMAIL ">";
-static char doc[] = "A small program to demonstrate using the command line interface.";
-
-int main (int argc, char **argv)
+Test(cli, test_short_option)
 {
-    struct arguments arguments;
-    arguments.mode = CHARACTER_MODE;
-    arguments.isCaseInsensitive = false;
-
- struct argp_option options[] =
- {
- { "foo", 'f', 0, 0, "Do something perhaps."},
- { 0 }
- };
- struct argp argp = { options, parse_opt, 0, doc};
- return argp_parse (&argp, argc, argv, 0, 0, &arguments);
+    initialise_params("--version");
+    struct argp_option options[] =
+        {
+        { "foo", 'f', 0, 0, "Do something perhaps."},
+        { 0 }
+        };
+    struct argp argp = {options, parse_opt};
+    argp_parse(&argp, params.argc, params.argv, 0, 0, &params.arguments);
 }

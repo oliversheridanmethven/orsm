8b9539289df0b37b72ea461c39ce5cb7579102c1
Dr Oliver Sheridan-Methven <oliver.sheridan-methven@hotmail.co.uk>
2023-07-28 12:16:01 +0100

    Deployed 694ac17 to v0.0.3 with MkDocs 1.5.1 and mike 1.1.2

---
+++ v0.0.3/src/sorting/my_sort.hpp
@@ -0,0 +1,56 @@
+
+#include <condition_variable>
+#include <mutex>
+#include <stack>
+#include <stdexcept>
+#include <thread>
+#include <utility>
+#include <vector>
+
+#ifndef MISC_MY_SORT_HPP
+#define MISC_MY_SORT_HPP
+
+// We will compare two implementations of quick sort.
+
+template<typename T>
+typename T::iterator quick_sort_partition(const typename T::iterator start, const typename T::iterator end)
+{
+    typename T::iterator pivot_value_ptr{end - 1};
+    typename T::iterator left_insertion_position{start};
+    for (typename T::iterator position{start}; position < pivot_value_ptr; position++)
+    {
+        if (*position <= *pivot_value_ptr)
+        {
+            std::swap(*left_insertion_position, *position);
+            left_insertion_position++;
+        }
+    }
+    std::swap(*left_insertion_position, *pivot_value_ptr);
+    return left_insertion_position;
+};
+
+template<typename T>
+void quick_sort(typename T::iterator start, typename T::iterator end)
+{
+    if (end - start > 1)
+    {
+        typename T::iterator partition = quick_sort_partition<T>(start, end);
+        quick_sort<T>(start, partition);
+        quick_sort<T>(partition, end);
+    }
+}
+
+template<typename T>
+void scalar_sort(typename T::iterator start, typename T::iterator end)
+{
+    quick_sort<T>(start, end);
+}
+
+typedef double T;
+typedef std::vector<T> container;
+typedef container::iterator iterator;
+typedef std::pair<iterator, iterator> sort_job;
+
+void parallel_sort(iterator start, iterator end);
+
+#endif//MISC_MY_SORT_HPP
---
+++ v0.0.3/src/testing/testing.h
@@ -0,0 +1,8 @@
+#ifndef TESTING_H_
+#define TESTING_H_
+
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+#include <criterion/redirect.h>
+
+#endif /*TESTING_H_*/

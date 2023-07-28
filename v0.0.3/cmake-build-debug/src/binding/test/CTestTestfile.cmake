2125d19fc147861e00ab3720f119bc7ec3044559
Dr Oliver Sheridan-Methven <oliver.sheridan-methven@hotmail.co.uk>
2023-07-28 12:58:36 +0200

    Deployed 8e3c61a with MkDocs version: 1.5.1

--- v0.0.3/cmake-build-debug/src/binding/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/oliver/ClionProjects/testing/src/binding/test
-# Build directory: /Users/oliver/ClionProjects/testing/cmake-build-debug/src/binding/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[binding_import]=] "/Library/Frameworks/Python.framework/Versions/3.11/bin/python3.11" "/Users/oliver/ClionProjects/testing/src/binding/test/binding_import.py")
-set_tests_properties([=[binding_import]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/oliver/ClionProjects/testing/CMakeLists.txt;63;add_test;/Users/oliver/ClionProjects/testing/CMakeLists.txt;55;add_python_test;/Users/oliver/ClionProjects/testing/src/binding/test/CMakeLists.txt;1;add_python_tests;/Users/oliver/ClionProjects/testing/src/binding/test/CMakeLists.txt;0;")

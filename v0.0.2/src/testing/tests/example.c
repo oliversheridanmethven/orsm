5a890eaae5e730da6f127dedc4ccd79a43c9a408
Dr Oliver Sheridan-Methven <oliver.sheridan-methven@hotmail.co.uk>
2023-07-28 11:29:58 +0100

    Deployed 973866f to v0.0.2 with MkDocs 1.5.1 and mike 1.1.2

---
+++ v0.0.2/src/testing/tests/example.c
@@ -0,0 +1,25 @@
+/* An example of using the Criterion testing framework. */
+
+#include "testing/testing.h"
+#include <string.h>
+
+/* This is what a failing test looks like. */
+//Test(example, test_failure) {
+//    cr_expect(eq(int,1,2), "This should fail.");
+//}
+
+Test(example, test_expect)
+{
+    cr_expect(strlen("Hello") == 5, "This should pass.");
+}
+
+Test(example, test_fail)
+{
+    cr_assert(eq(int, 2, 1 + 1), "This should pass.");
+    cr_assert(not(eq(int, 1, 2)), "This should pass.");
+}
+
+Test(logging, test_exit, .exit_code = 1)
+{
+    exit(1);
+}

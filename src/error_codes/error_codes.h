6196cb5faf0155357f1bf263e2cbacec9329bc8a
Dr Oliver Sheridan-Methven <oliver.sheridan-methven@hotmail.co.uk>
2023-07-28 10:47:48 +0200

    Comment added.

--- src/error_codes/error_codes.h
+++ src/error_codes/error_codes.h
@@ -1,6 +1,11 @@
 #ifndef TESTING_ERROR_CODES_H
 #define TESTING_ERROR_CODES_H
 
+/* If you would like to hack together stricter type safety,
+ * and start to mimic a C++ enum class, then see the advice
+ * listed here:
+ * cf. https://stackoverflow.com/q/43043246/5134817
+ * */
 typedef enum error_code
 {
     EC_SUCCESS = 0,

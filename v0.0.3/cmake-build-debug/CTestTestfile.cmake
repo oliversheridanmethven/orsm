811aa299e61456e8e4b8b1f398137622b3cfab4c
Dr Oliver Sheridan-Methven <oliver.sheridan-methven@hotmail.co.uk>
2023-07-28 13:15:49 +0200

    Deployed 694ac17 with MkDocs version: 1.5.1

--- v0.0.3/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/oliver/ClionProjects/testing
-# Build directory: /Users/oliver/ClionProjects/testing/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("foo")
-subdirs("hello")
--- v0.0.3/cmake-build-debug/src/logging/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/oliver/ClionProjects/testing/src/logging/tests
-# Build directory: /Users/oliver/ClionProjects/testing/cmake-build-debug/src/logging/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[logging_test]=] "/Users/oliver/ClionProjects/testing/cmake-build-debug/src/logging/tests/logging_test")
-set_tests_properties([=[logging_test]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/oliver/ClionProjects/testing/src/logging/tests/CMakeLists.txt;3;add_test;/Users/oliver/ClionProjects/testing/src/logging/tests/CMakeLists.txt;0;")

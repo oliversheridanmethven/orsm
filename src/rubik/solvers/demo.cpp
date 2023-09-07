49cb2d1e913b5528741279de1123fbe60f69a604
Dr Oliver Sheridan-Methven <oliver.sheridan-methven@hotmail.co.uk>
2023-09-07 14:11:24 +0200

    Neated up the CLI.

--- src/rubik/solvers/demo.cpp
+++ src/rubik/solvers/demo.cpp
@@ -4,53 +4,23 @@
 #include "rubik/shapes/volume.hpp"
 #include "rubik/solvers/meet_in_middle_recursive.hpp"
 #include "rubik/paths/paths.hpp"
-#include "boost/program_options.hpp"
+#include "common/cli.hpp"
 #include <ranges>
 
 int main(int argc, char **argv) {
     int seed;
     unsigned int turns;
 
-    namespace po = boost::program_options;
-    po::options_description desc("Allowed options");
-    po::arg = "";
-    desc.add_options()
-            ("help", "Produce this help message.")
-            ("turns", po::value<decltype(turns)>(&turns)->default_value(100)/*->value_name("TURNS")*/,
+    auto [options, positional_options, parser_finalise] = cli::setup_standard_parser(argc, argv,
+                                                                                     "Demonstration of solving Rubik style puzzles.");
+
+    options.add_options()
+            ("turns",
+             cli::po::value<decltype(turns)>(&turns)->default_value(100)/*->value_name("TURNS")*/,
              "The number of turns.")
-            ("verbose", po::bool_switch()->default_value(false), "Set verbose logging level.")
-            ("debug", po::bool_switch()->default_value(false), "Set debug logging level.")
-            ("trace", po::bool_switch()->default_value(false), "Set trace logging level.")
-            ("seed", po::value(&seed), "The seed.");
+            ("seed", cli::po::value(&seed), "The seed.");
 
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    if (vm["verbose"].as<bool>()) {
-        FLAGS_v = INFO_LEVEL;
-        FLAGS_logtostdout = true;
-    }
-
-    if (vm["debug"].as<bool>()) {
-        FLAGS_v = DEBUG_LEVEL;
-        FLAGS_logtostdout = true;
-    }
-
-    if (vm["trace"].as<bool>()) {
-        FLAGS_v = TRACE_LEVEL;
-        FLAGS_logtostdout = true;
-    }
-
-
-    google::InitGoogleLogging(argv[0]);
-
-    if (vm.count("help")) {
-        std::cout << "Demonstration of the rubik functionality." << "\n\n"
-                  << desc << "\n";
-        exit(EXIT_SUCCESS);
-    }
+    parser_finalise(options, positional_options);
 
 
     Volume shape;

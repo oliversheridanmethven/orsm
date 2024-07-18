fe5748d94e3b3a69bc8a335f6e1a0f8f659639c4
Dr Oliver Sheridan-Methven <oliver.sheridan-methven@hotmail.co.uk>
2024-07-19 00:23:52 +0100

    Deployed 201b46d with MkDocs version: 1.6.0

--- src/orsm/tests/test_repo.py
+++ src/orsm/tests/test_repo.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import unittest
 from orsm.variables import variable_names_and_objects
-from orsm import version
+from orsm import repo
 
 
 class VersionInfo(unittest.TestCase):
 
-    def test_basic_version_info(self):
-        for variable, get_string_function in variable_names_and_objects(version.repo_name, version.repo_author, version.repo_version, version.repo_email):
+    def test_basic_repo_info(self):
+        for variable, get_string_function in variable_names_and_objects(repo.repo_name, repo.repo_author, repo.repo_version, repo.repo_email):
             output = get_string_function()
             self.assertIsInstance(output, str, msg=f"The {variable = } with {output = } is not the right type.")
             self.assertTrue(output, msg=f"The {variable = } with {output = } is meant to be non-trivial.")
